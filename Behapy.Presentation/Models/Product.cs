5366da4a1382956107fcf116f1a808f2c6b0efd6
An Nguyen <annguyen.it.utc@gmail.com>
2023-08-02 23:15:26 +0700

    feat: add admin layout

--- Behapy.Presentation/Models/Product.cs
+++ Behapy.Presentation/Models/Product.cs
@@ -1,29 +1,45 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Behapy.Presentation.Models
+namespace Behapy.Presentation.Models;
+
+public class Product
 {
-	public class Product
-	{
-		public int Id { get; set; }
-		public string Name { get; set; } = null!;
+	public int Id { get; set; }
 
-		[Precision(18, 2)]
-		public decimal Price { get; set; }
-		public bool IsActive { get; set; }
-		public string Description { get; set; } = null!;
-		public string ImageUrl { get; set; } = null!;
+	[Display(Name = "Tên sản phẩm")]
+	public string Name { get; set; } = null!;
 
-		[Precision(18, 2)]
-		public decimal Discount { get; set; }
-		public DateTime CreatedAt { get; set; }
+	[Precision(18, 2)]
+	[Display(Name = "Giá tiền")]
+	public decimal Price { get; set; }
 
-		public int CategoryId { get; set; }
-		public Category Category { get; set; } = null!;
+	[Display(Name = "Kích hoạt")]
+	public bool IsActive { get; set; }
+	
+	[Display(Name = "Mô tả")]
+	public string Description { get; set; } = null!;
+	
+	[Display(Name = "Ảnh")]
+	public string ImageUrl { get; set; } = null!;
 
-		public int? PromotionId { get; set; }
-		public Promotion? Promotion { get; set; }
+	[Precision(18, 2)]
+	[Display(Name = "Giảm giá")]
+	public decimal Discount { get; set; }
 
-		public List<OrderDetail> OrderDetails { get; set; } = new();
-		public List<CartItem> CartItems { get; set; } = new();
-	}
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	[Display(Name = "Thời gian tạo")]
+	public DateTime CreatedAt { get; set; }
+
+	[Display(Name = "Danh mục")]
+	public int CategoryId { get; set; }
+	public Category Category { get; set; } = null!;
+
+	[Display(Name = "Khuyến mãi")]
+	public int? PromotionId { get; set; }
+	public Promotion? Promotion { get; set; }
+
+	public List<OrderDetail> OrderDetails { get; set; } = new();
+	public List<CartItem> CartItems { get; set; } = new();
 }
