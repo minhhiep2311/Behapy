1cd9df829d856d59d33b889de4f85d6653003674
An Nguyen <annguyen.it.utc@gmail.com>
2023-08-07 22:13:38 +0700

    feat: add script to seed data

---
+++ Behapy.Presentation/Extensions/StringHelper.cs
@@ -0,0 +1,6 @@
+﻿namespace Behapy.Presentation.Extensions;
+
+public static class StringExtension
+{
+    public static string ToTitle(this string str) => char.ToUpper(str[0]) + str[1..].ToLower();
+}
