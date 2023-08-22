9c8ecdfedea676a35f4cc2cc4080e0de2de195b8
An Nguyen <annguyen.it.utc@gmail.com>
2023-08-22 21:18:21 +0700

    feat: register

--- Behapy.Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Behapy.Presentation/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -6,13 +6,9 @@
 using Behapy.Presentation.Areas.Identity.Data;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
 
 namespace Behapy.Presentation.Areas.Identity.Pages.Account;
 
@@ -21,23 +17,24 @@ public class RegisterModel : PageModel
     private readonly SignInManager<User> _signInManager;
     private readonly UserManager<User> _userManager;
     private readonly IUserStore<User> _userStore;
-    private readonly IUserEmailStore<User> _emailStore;
+    // private readonly IUserEmailStore<User> _emailStore;
     private readonly ILogger<RegisterModel> _logger;
-    private readonly IEmailSender _emailSender;
+    // private readonly IEmailSender _emailSender;
 
     public RegisterModel(
         UserManager<User> userManager,
         IUserStore<User> userStore,
         SignInManager<User> signInManager,
-        ILogger<RegisterModel> logger,
-        IEmailSender emailSender)
+        ILogger<RegisterModel> logger
+        // IEmailSender emailSender
+        )
     {
         _userManager = userManager;
         _userStore = userStore;
-        _emailStore = GetEmailStore();
+        // _emailStore = GetEmailStore();
         _signInManager = signInManager;
         _logger = logger;
-        _emailSender = emailSender;
+        // _emailSender = emailSender;
     }
 
     /// <summary>
@@ -113,30 +110,38 @@ public class RegisterModel : PageModel
 
         if (!ModelState.IsValid) return Page();
 
-        var user = CreateUser();
+        var user = new User
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = Input.Email,
+            Email = Input.Email,
+            FullName = Input.UserName
+        };
 
         await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-        await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+        // await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
         var result = await _userManager.CreateAsync(user, Input.Password);
 
         if (result.Succeeded)
         {
             _logger.LogInformation("User created a new account with password");
 
-            var userId = await _userManager.GetUserIdAsync(user);
-            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-            var callbackUrl = Url.Page(
-                "/Account/ConfirmEmail",
-                pageHandler: null,
-                values: new { area = "Identity", userId, code, returnUrl },
-                protocol: Request.Scheme);
+            // var userId = await _userManager.GetUserIdAsync(user);
+            // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            // var callbackUrl = Url.Page(
+            //     "/Account/ConfirmEmail",
+            //     pageHandler: null,
+            //     values: new { area = "Identity", userId, code, returnUrl },
+            //     protocol: Request.Scheme);
 
-            if (callbackUrl != null)
-            {
-                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-            }
+            // if (callbackUrl != null)
+            // {
+            //     await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+            //         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+            // }
+
+            await _userManager.AddToRoleAsync(user, "User");
 
             if (_userManager.Options.SignIn.RequireConfirmedAccount)
             {
@@ -154,20 +159,6 @@ public class RegisterModel : PageModel
 
         // If we got this far, something failed, redisplay form
         return Page();
-    }
-
-    private User CreateUser()
-    {
-        try
-        {
-            return Activator.CreateInstance<User>();
-        }
-        catch
-        {
-            throw new InvalidOperationException($"Can't create an instance of '{nameof(User)}'. " +
-                                                $"Ensure that '{nameof(User)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                                                $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
-        }
     }
 
     private IUserEmailStore<User> GetEmailStore()
