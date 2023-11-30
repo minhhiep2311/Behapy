631542ac480073a622bd20a0f39f4449af202029
An Nguyen <annguyen.it.utc@gmail.com>
2023-11-30 20:19:22 +0700

    feat: create user when add employee

---
+++ Behapy.Presentation/ViewModels/CreateEmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Behapy.Presentation.ViewModels;
+
+public class CreateEmployeeModel
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Họ tên")]
+    public string FullName { get; set; } = null!;
+
+    [Display(Name = "Địa chỉ")]
+    public string Address { get; set; } = null!;
+
+    [Display(Name = "Vị trí")]
+    public string Position { get; set; } = null!;
+
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+}
